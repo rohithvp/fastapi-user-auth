8f9970087f433f3b10bfc4134d91270eee0884b2
Mahapooja66 <mahapooja66@gmail.com>
2025-05-10 14:09:43 +0530

    Version 7

--- fastapi-user-auth/app/database.py
+++ fastapi-user-auth/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import  create_engine
+'''from sqlalchemy import  create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -8,4 +8,4 @@ engine= create_engine(database_url)
 
 sessionlocal= sessionmaker(autocommit=False,autoflush=False ,bind=engine)
 
-Base =  declarative_base()
+Base =  declarative_base()'''
