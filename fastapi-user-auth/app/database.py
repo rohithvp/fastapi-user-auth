776358647ddd1c99975efc529020ca91a3ac939e
rohithvp <rohithpandian13@gmail.com>
2025-06-12 00:10:44 +0530

    corrected the database

--- fastapi-user-auth/app/database.py
+++ fastapi-user-auth/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import  create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-database_url =  "postgresql://postgres:password@172.17.0.1:5432/users"
+database_url =  "postgresql://postgres:password@172.17.0.1:5432/postgres"
 
 engine= create_engine(database_url)
 
